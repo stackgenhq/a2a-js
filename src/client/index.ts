c44f084f266b39c38204a829d8a4c5248a6d630d
Mike Prince <mike@mikeprince.com>
2025-08-07 12:13:49 -0700

    Exported A2AClientOptions

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,5 +2,5 @@
  * Client entry point for the A2A Server V2 library.
  */
 
-export { A2AClient } from "./client.js";
+export { A2AClient, A2AClientOptions } from "./client.js";
 export * from "./auth-handler.js";
